5d92bc514742548ef5ad1eae0917302299e42afe
fabri2605 <fabridp77@gmail.com>
2022-07-18 13:37:24 -0300

    scroll none

--- components/PagesList.js
+++ components/PagesList.js
@@ -3,7 +3,7 @@ import PageItem from './PageItem';
 const PagesList = (props) => {
     return (
         <div className='centered'>
-            <h2>This are some pages I created while learning..</h2>
+            <h2>Hi, my name is Fabricio and this are some pages I created while learning..</h2>
             {props.pages.map((e) => {
                 return (
                     <PageItem
