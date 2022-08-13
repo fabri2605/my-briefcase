34542444656420173fe413098bb38ab5a776d885
fabri2605 <fabridp77@gmail.com>
2022-08-13 12:03:39 -0300

    about-fixed

--- components/MyBody.js
+++ components/MyBody.js
@@ -12,10 +12,12 @@ const myBody = () => {
                         <li>Java fullstack using Spring Boot</li>
                         <li>Javascript with JQuery</li>
                         <li>
-                            Actually working on a React course with applications
-                            of NextJs %70
+                            React with NextJS or React-router
                         </li>
-                        <li>MySQL & MongoDB </li>
+                        <li>
+                            TypeScript as a secondary language
+                        </li>
+                        <li>MySQL , MongoDB & firebase</li>
                         <li>GitHub</li>
                     </ul>
                 </div>
@@ -29,8 +31,10 @@ const myBody = () => {
                     </ul>
                 </div>
                 <div className={classes.contact}>
+                    <h3>Contact me</h3>
                     <ul>
-                        <li>+54 2616149941 fabridp77@gmail.com</li>
+                        <li>+54 2616149941</li>
+                        <li>fabridp77@gmail.com</li>
                     </ul>
                 </div>
             </div>
